6938caf809b03db39bb37a0b1923c681d882983a
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-06-28 00:34:43 -0300

    Cambios en la logica de las comidas
    cambios

---
+++ src/serviciosAlPasajero/BebidasGeneral.java
@@ -0,0 +1,14 @@
+package serviciosAlPasajero;
+
+public class BebidasGeneral 
+{
+	private String nombre;
+	private int valor;
+	
+	public BebidasGeneral(String nombreRecib, int valorRecib) 
+	{	
+		nombre = nombreRecib;
+		valor = valorRecib;
+	}
+
+}
