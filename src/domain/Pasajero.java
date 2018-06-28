3e62279185d33115329e182bc4a061b747e478b7
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-06-27 21:30:27 -0300

    Agruegue atributo en pasajero
    estadoPasajero; // 0 = ya se fue / 1 = todavia no llego / 2 = esta en el hotel

--- src/domain/Pasajero.java
+++ src/domain/Pasajero.java
@@ -21,6 +21,7 @@ public class Pasajero
 	private String nacionalidad;
 	private int idPasajero;
 	private Integer CantidadPersona;
+	private Integer estadoPasajero; // 0 = ya se fue / 1 = todavia no llego / 2 = esta en el hotel
 	HashMap<Date,Habitacion> habitacion;
 	
 	
