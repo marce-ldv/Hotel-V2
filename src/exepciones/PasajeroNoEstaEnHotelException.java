cf8e7b4f8b9ee51c90cfdec080943aff61f16ab7
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-06-29 00:41:03 -0300

    Excepcion
    Excepcion

--- src/exepciones/PasajeroNoEstaEnHotelException.java
+++ src/exepciones/PasajeroNoEstaEnHotelException.java
@@ -1,5 +1,13 @@
 package exepciones;
 
+/**
+ * 
+ * Esta excepcion se utiliza cuando el pasajero realiza una operacion la cual el sistema necesita que se encuentre en el hotel  
+ * 
+ * @author Ignacio Chiaradia
+ * */
+
+
 public class PasajeroNoEstaEnHotelException extends Exception 
 {
 	public PasajeroNoEstaEnHotelException(String msg)
